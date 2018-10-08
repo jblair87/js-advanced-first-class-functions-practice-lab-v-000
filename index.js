9e057b38a0d7e8353707821abfabeeccbae715cf
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 15:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,11 @@ return drivers.slice().sort(function(driverOne, driverTwo)
 
 
 const totalRevenue = function (driver)  
-return drivers.reduce(function (total, currentDriver) {
-    return currentDriver.revenue + total; }, 0);
+return drivers.reduce(function (total, currentDriver) 
+{ return currentDriver.revenue + total; }, 0);
 };
+
+
+
 const averageRevenue = function (driver){};
 
