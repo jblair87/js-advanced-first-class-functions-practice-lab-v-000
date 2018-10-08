9d99b91608b567148a94cca0c6990fca91bb1f07
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 14:53:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+const logDriverNames = function (drivers){
+    const names = [];
+drivers.forEach(function (driver){
+names.push(driver.name);
+});
+return names;
+};
+logDriverNames(drivers);
+
+const logDriversByHometown = function (driver, hometown){};
+const driversByRevenue = function (driver){};
+
+const driversByName = function (driver){};
+
+const totalRevenue = function (driver){};
+
+const averageRevenue = function (driver){};
+
