17c9725b5c2fe42265e863b53ec9451b668505ec
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 14:55:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const logDriverNames = function (drivers){
-    const names = [];
 drivers.forEach(function (driver){
 console.log(driver.name);
 });
-return names;
 };
-
 
 const logDriversByHometown = function (driver, hometown){};
 const driversByRevenue = function (driver){};
