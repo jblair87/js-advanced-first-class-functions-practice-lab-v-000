71ca12a86523d7c690ce19d52865530d89a36c22
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 14:55:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ console.log(driver.name);
 });
 };
 
-const logDriversByHometown = function (driver, hometown){};
+const logDriversByHometown = function (driver, hometown){
+ drivers.forEach(function (driver){
+console.log(driver.name);
+}); 
+};
 const driversByRevenue = function (driver){};
 
 const driversByName = function (driver){};
